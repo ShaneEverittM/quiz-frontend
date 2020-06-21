cdccdd259d955a2d58c98b43e15e70503f92199c
anthonyleol <anthony.lisa0@gmail.com>
2020-06-21 12:10:11 -0700

    basic quiz logic structure

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
+import { Redirect } from "react-router-dom"
 
 const serverURL = "https://jsonplaceholder.typicode.com/posts/1"
-const serverURL2 = "localhost:8000/"
+const serverURL2 = "http://localhost:8000/"
+
 const data = [{
     q: "fill in the blank: Lorem ______",
     pa: [
@@ -16,12 +18,15 @@ const data = [{
     ],
     a: 3
 }]
+
 class QuizPage extends React.Component {
     state = {
         title: "loading",
         description: "loading",
         questions: [],
-        curQuestion: 0
+        curQuestion: 0,
+        numQuestions: 2,
+        redirectToResults: false
     }
 
     componentDidMount() {
@@ -31,29 +36,62 @@ class QuizPage extends React.Component {
     //TODO get question list from backend, display one at a time
 
     testAPI = () => {
-
-        fetch(serverURL).then(response => response.json())
+        fetch(serverURL)
+            .then(response => response.json())
             .then(json => { this.setState({ title: json.title, description: json.body }) })
-        fetch(serverURL2).then(response => console.log(response)).catch(() => console.log("we tried our best"))
-    }
 
+        //fetch(serverURL2).then(response => response.json()).then(json => console.log(json)).catch(() => console.log("we tried our best"))
+    }
+    renderNextQuestion = () => {
+        if (this.state.curQuestion < this.state.numQuestions)
+            this.setState({ questions: [...this.state.questions, data[this.state.curQuestion]], curQuestion: this.state.curQuestion + 1 })
+
+    }
+    redirect = () => {
+        console.log("running")
+        if (this.state.redirectToResults) {
+            return <Redirect to="/placeholder" />
+        }
+    }
+    setRedirect = () => {
+        this.setState({ redirectToResults: true })
+    }
+    renderButtonText = () => {
+        let buttonText = "Next"
+        let handle = this.renderNextQuestion;
+
+        if (this.state.curQuestion === 0)
+            buttonText = "Start"
+
+        else if (this.state.curQuestion === this.state.numQuestions) {
+            buttonText = "end";
+            handle = this.setRedirect;
+        }
+        return (
+            <button onClick={handle}> {buttonText}</button>
+        )
+    }
     render() {
         return (
             <div>
                 <h3>Title: {this.state.title}</h3>
                 <p>This is a quiz:{this.state.description}</p>
-                <button onClick={() => this.setState({ questions: [...this.state.questions, data[this.state.curQuestion]], curQuestion: this.state.curQuestion + 1 })}> Start</button>
                 {
                     this.state.questions.map((item, i) => {
                         return <div key={i}>
                             <h4>{item.q}</h4>
                             {item.pa.map((item, j) => {
-                                return <div key={j}><input type="radio" id={item} name={i} /> <label htmlFor={item}>{item}</label> </div>
+                                return <div key={j}>
+                                    <input type="radio" id={item} name={i} />
+                                    <label htmlFor={item}>{item}</label>
+                                </div>
 
                             })}
                         </div>
                     })
                 }
+                {this.renderButtonText()}
+                {this.redirect()}
             </div >
         )
     }
