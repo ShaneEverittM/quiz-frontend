4a61f395b8887ccefa23c4c53c4e7434235f3627
Anthony <anthony.lisa0@gmail.com>
2020-07-09 15:54:53 -0700

    basic home display with api data

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Quiz.css";
 
-const Quiz = (props) => {
+const Quiz = ({ title, description }) => {
   return (
     <div className="quiz-box">
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CategoryPreview from "../components/CategoryPreview";
 import "./Home.css";
 import { getQuizPreviews } from "../api/api.js";
 
 let quiz_list = ["quiz1", "quiz2", "quiz3", "quiz1", "quiz2", "quiz3"];
 const Home = () => {
+  let [quizzes, setQuizzes] = useState([]);
   useEffect(() => {
-    quiz_list = getQuizPreviews();
-  });
+    async function fetchData() {
+      const response = await getQuizPreviews();
+      console.log("response: ", response);
+      setQuizzes(response.data);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="home-container">
       <CategoryPreview
-        quizList={quiz_list}
+        quizList={quizzes}
         categoryName="Top quizzes last hour"
       />
       <CategoryPreview
-        quizList={quiz_list}
+        quizList={quizzes}
         categoryName="Picked esspecially for you"
       />
-      <CategoryPreview
-        quizList={quiz_list}
-        categoryName="Our spiciest quizzes"
-      />
+      <CategoryPreview quizList={quizzes} categoryName="Our spiciest quizzes" />
     </div>
   );
 };
